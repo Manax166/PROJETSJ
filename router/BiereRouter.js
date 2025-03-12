47c997a8644a0bae4fcb60c92c865d7ce2231468
pripault <paul.ripault@gmail.com>
2025-03-12 16:27:15 +0100

    add BiereRequest middleware for data validation

--- router/BiereRouter.js
+++ router/BiereRouter.js
@@ -6,9 +6,10 @@ const {
   getBeers,
   getBeerById,
 } = require("../controllers/BeerController");
+const BiereRequest = require("../middleware/BiereRequest");
 
-router.post("/bars/:id_bar/biere", addBeer);
-router.put("/biere/:id_biere", updateBeer);
+router.post("/bars/:id_bar/biere", BiereRequest,addBeer);
+router.put("/biere/:id_biere", BiereRequest,updateBeer);
 router.delete("/biere/:id_biere", deleteBeer);
 router.get("/bars/:id_bar/biere", getBeers);
 router.get("/biere/:id_biere", getBeerById);

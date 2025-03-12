8a53258284dc0c1bb79bf543cc0d5afecd279193
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-03-12 17:01:23 +0100

    add cascading delete

--- controllers/BiereCommandeController.js
+++ controllers/BiereCommandeController.js
@@ -1,34 +1,35 @@
-const {Biere} = require("../models/index");
-const {Commande} = require("../models/index");
+const { Biere } = require("../models/index");
+const { Commande } = require("../models/index");
 
 const addBeerIncommand = async (req, res) => {
   try {
-    const id_Com = parseInt(req.params.id_commande)
-    const id_Biere = parseInt(req.params.id_biere)
+    const id_Com = parseInt(req.params.id_commande);
+    const id_Biere = parseInt(req.params.id_biere);
+
+    console.log(id_Com);
+    console.log(id_Biere);
 
     const [biere, commande] = await Promise.all([
       Biere.findByPk(id_Biere),
-      Commande.findByPk(id_Com)
-    ])
-    await commande.addBiere(biere)
-  } catch(err) {
+      Commande.findByPk(id_Com),
+    ]);
+    await commande.addBiere(biere);
+  } catch (err) {
     console.log("erreur " + err);
-    
   }
 };
 const deleteBeerInCommand = async (req, res) => {
   try {
-    const id_Com = parseInt(req.params.id_commande)
-    const id_Biere = parseInt(req.params.id_biere)
+    const id_Com = parseInt(req.params.id_commande);
+    const id_Biere = parseInt(req.params.id_biere);
 
     const [biere, commande] = await Promise.all([
       Biere.findByPk(id_Biere),
-      Commande.findByPk(id_Com)
-    ])
-    await commande.removeBiere(biere)
-  } catch(err) {
+      Commande.findByPk(id_Com),
+    ]);
+    await commande.removeBiere(biere);
+  } catch (err) {
     console.log("erreur " + err);
-    
   }
 };
 

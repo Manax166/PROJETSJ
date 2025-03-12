f005811577fd447d8c727e696961efae62f99a88
Adam HEMAMOU <h-mamou@hotmail.fr>
2025-03-12 10:53:54 +0100

    add Command Controller

--- controllers/CommandeController.js
+++ controllers/CommandeController.js
@@ -1,14 +1,107 @@
-const Commande = require("../models/index")
-const Biere = require("../models/index")
+const { Bar, Commande, Biere } = require("../models/index");
 
-const addOrder = (req, res) =>{}
+const addOrder = async (req, res) => {
+  const { id_bar } = req.params;
+  if (!id_bar) {
+    return res
+      .status(400)
+      .json({ msg: "L'ID du bar est manquant dans l'URL." });
+  }
 
-const updateOrder = (req, res) =>{}
+  try {
+    const bar = await Bar.findByPk(id_bar);
 
-const deleteOrder = (req, res) =>{}
+    if (!bar) {
+      return res.status(400).json({ msg: "Ce bar n'existe pas" });
+    }
 
-const getAllOrdersFromBar = (req, res) =>{}
+    const data = { ...req.body, bar_id: id_bar };
 
-const getOrder = (req, res) =>{}
+    const result = await Commande.create(data);
 
-module.exports = {addOrder, updateOrder, deleteOrder, getAllOrdersFromBar, getOrder}
+    res.status(200).json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Un problème est survenue", error: error.message });
+  }
+};
+
+const updateOrder = async (req, res) => {
+  const { id_commande } = req.params;
+
+  try {
+    const commande = await Commande.findByPk(id_commande);
+    if (!commande) {
+      return res.status(404).json({ msg: "Cette commande n'existe pas." });
+    }
+
+    await commande.update(req.body);
+    res.status(200).json({ msg: "Commande mise à jour avec succès", commande });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Un problème est survenu", error: error.message });
+  }
+};
+
+const deleteOrder = async (req, res) => {
+  const { id_commande } = req.params;
+
+  try {
+    const commande = await Commande.findByPk(id_commande);
+    if (!commande) {
+      return res.status(404).json({ msg: "Cette commande n'existe pas." });
+    }
+
+    await commande.destroy();
+    res.status(200).json({ msg: "Commande supprimée avec succès" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Un problème est survenu", error: error.message });
+  }
+};
+
+const getAllOrdersFromBar = async (req, res) => {
+  const { id_bar } = req.params;
+
+  try {
+    const bar = await Bar.findByPk(id_bar, { include: Commande });
+
+    if (!bar) {
+      return res.status(404).json({ msg: "Ce bar n'existe pas." });
+    }
+
+    res.status(200).json(bar.commandes);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Un problème est survenu", error: error.message });
+  }
+};
+
+const getOrder = async (req, res) => {
+  const { id_commande } = req.params;
+
+  try {
+    const commande = await Commande.findByPk(id_commande);
+    if (!commande) {
+      return res.status(404).json({ msg: "Cette commande n'existe pas." });
+    }
+
+    res.status(200).json(commande);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "Un problème est survenu", error: error.message });
+  }
+};
+
+module.exports = {
+  addOrder,
+  updateOrder,
+  deleteOrder,
+  getAllOrdersFromBar,
+  getOrder,
+};
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 app.listen(PORT, () => console.log(`App Running on port ${PORT}`));
 
-app.use( require( "./router/BarRouter"))
-app.use( require( "./router/BiereRouter"))
-app.use( require( "./router/CommandeRouter"))
-app.use( require( "./router/BiereCommandeRouter"))
+app.use(require("./router/BarRouter"));
+app.use(require("./router/BiereRouter"));
+app.use(require("./router/CommandeRouter"));
+app.use(require("./router/BiereCommandeRouter"));
